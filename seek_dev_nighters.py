4b1195ab3c27e85aa1cb6e7da211332484e85877
mfgnik <31517000+mfgnik@users.noreply.github.com>
2018-03-21 12:38:16 +0300

    Update seek_dev_nighters.py

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,53 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+from datetime import datetime
 
-def get_midnighters():
-    pass
+def get_amount_pages():
+    url = 'https://devman.org/api/challenges/solution_attempts/'
+    response = requests.get(
+        url=url,
+        params={
+            'page': 1
+        }
+    )
+    return response.json()['number_of_pages']
+
+
+def load_attempts():
+    pages = get_amount_pages()
+    url = 'https://devman.org/api/challenges/solution_attempts/'
+    for page in range(1, pages + 1):
+        response = requests.get(
+            url=url,
+            params={
+                'page': page
+            }
+        ).json()
+        attempts_list = response['records']
+        for attempt in attempts_list:
+            yield attempt
+
+
+def is_user_midnighter(attempt):
+    timezone = pytz.timezone(attempt['timezone'])
+    time = datetime.fromtimestamp(attempt['timestamp'], tz=timezone)
+    end_of_night = 8
+    return time.hour <= end_of_night
+
+
+def get_midnighters(attempts):
+    midnighters = set()
+    for attempt in attempts:
+        if is_user_midnighter(attempt):
+            midnighters.add(attempt['username'])
+    return midnighters
+
 
 if __name__ == '__main__':
-  pass
+    midnighters = get_midnighters(load_attempts())
+    if len(midnighters) == 0:
+        print('There is no midnighters')
+    else:
+        print('List of midnighters:')
+        for midnighter in midnighters:
+            print(midnighter)
